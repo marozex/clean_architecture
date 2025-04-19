7283087c0ed31aaa98f141f97364b97b9beff71b
島倉健麿 <>
2025-04-19 16:38:12 +0900

    エンドポイント追加

--- library/README.md
+++ library/README.md
@@ -27,3 +27,11 @@ vi schema.prisma // モデル定義追加
 npx prisma db push // DBスキーマ更新
 npx prisma generate // 型定義生成
 ```
+
+### リクエストテスト
+```
+curl --json '{"title":"入門"}' http://localhost:3000/books
+
+curl http://localhost:3000/books/b58d0bb0-bfd4-4c34-b83e-728c68db7729 //一つ前のコマンドで登録したレコードのIDを指定
+curl http://localhost:3000/books/hogehoge //存在しないIDなのでエラー
+```
--- library/src/app.ts
+++ library/src/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import { BookcController } from "./presentation/bookController";
 
 const app = express();
 
 app.use(express.json());
 
+const bookcController = new BookcController();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello express" });
-});
+app.post("/books", bookcController.add.bind(bookcController));
+app.get("/books/:id", bookcController.findById.bind(bookcController));
+
+// 動作確認用サンプルエンドポイント
+// app.get("/", (req, res) => {
+//   res.json({ message: "hello express" });
+// });
 
 app.listen(PORT, () => console.log("Server is running"));

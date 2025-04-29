f86e988ea94b3f1bbbba769f0e44f593a6d7bff2
島倉健麿 <>
2025-04-29 15:25:26 +0900

    エンドポイント追加

--- library/src_layered_practice/app.ts
+++ library/src_layered_practice/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { BookController } from "./presentation/bookController";
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3322;
+
+const bookController = new BookController();
+
+app.post("/books", bookController.add.bind(bookController));
+app.get("/books/:id", bookController.findById.bind(bookController));
 
 app.get("/", (req, res) => {
   res.json({ message: "hello practice" });

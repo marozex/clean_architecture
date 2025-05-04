4e9ccdcc88f0851923759df84b895ce8d86e01ae
marozex <49114932+marozex@users.noreply.github.com>
2025-05-04 22:56:03 +0900

    404と500のミドルウェア追加

--- reversi/src/main.ts
+++ reversi/src/main.ts
@@ -17,6 +17,24 @@ app.get("/api/error", async (req, res) => {
   throw new Error("error occurred");
 });
 
+// app.use()はミドルウェアであり、app.listen()より前に書く
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: {
+      message: "not found",
+    },
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`reversi app started: http://localhost:${PORT}`);
 });
